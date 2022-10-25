43ea0143ea9f0c27ae8641e0fcd6702b68d8e3f5
Scott Chow <scottchowww@gmail.com>
2022-10-25 13:24:44 +0800

    implemented localStorage

--- index.js
+++ index.js
@@ -20,9 +20,28 @@ function renderMenu() {
 	document.getElementById('menu').innerHTML += getMenuHtml()
 }
 
-document.addEventListener('click', (e) => {
-	if (e.target.dataset.foodid) {
-		console.log(e.target.dataset.foodid)
+function handleAddToBucketClick(evt) {
+	if (!localStorage.hasOwnProperty('order')) {
+		localStorage.setItem('order', JSON.stringify([]))
+	}
+	const orderObjArray = JSON.parse(localStorage.getItem('order'))
+	const orderObj = orderObjArray.filter((orderObj) => {
+		return orderObj.id === evt.target.dataset.foodid
+	})
+	if (orderObj.length === 1) {
+		orderObj[0].number++
+	} else {
+		orderObjArray.push({
+			id: evt.target.dataset.foodid,
+			number: 1 
+		})
+	}
+	localStorage.setItem('order', JSON.stringify(orderObjArray))
+}
+
+document.addEventListener('click', (evt) => {
+	if (evt.target.dataset.foodid) {
+		handleAddToBucketClick(evt)
 	}
 })
 
